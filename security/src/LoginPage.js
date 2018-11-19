037d756f818122c9e73dc787d6d8dd61c2386f81
Carey <careycbaldwin@gmail.com>
2018-11-19 14:08:19 -0500

    built basic login page for testing

---
+++ security/src/LoginPage.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class LoginPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: '',
+      error: '',
+    };
+
+    this.handlePassChange = this.handlePassChange.bind(this);
+    this.handleUserChange = this.handleUserChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.dismissError = this.dismissError.bind(this);
+  }
+
+  dismissError() {
+    this.setState({ error: '' });
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+
+    if (!this.state.username) {
+      return this.setState({ error: 'Username is required' });
+    }
+
+    if (!this.state.password) {
+      return this.setState({ error: 'Password is required' });
+    }
+
+    return this.setState({ error: '' });
+  }
+
+  handleUserChange(evt) {
+    this.setState({
+      username: evt.target.value,
+    });
+  };
+
+  handlePassChange(evt) {
+    this.setState({
+      password: evt.target.value,
+    });
+  }
+
+  render() {
+    // NOTE: use data-attributes for easier E2E testing
+
+    return (
+      <div className="Login">
+      <h4>
+         Please enter your credentials below.
+        </h4>
+        <form onSubmit={this.handleSubmit}>
+          {
+            this.state.error &&
+            <h3 data-test="error" onClick={this.dismissError}>
+              <button onClick={this.dismissError}>✖</button>
+              {this.state.error}
+            </h3>
+          }
+          <input 
+          type="text" 
+          data-test="username" 
+          value={this.state.username} 
+          onChange={this.handleUserChange} 
+          placeholder = "Username"
+          />
+
+          <input 
+          type="password" 
+          data-test="password" 
+          value={this.state.password} 
+          onChange={this.handlePassChange}
+          placeholder = "Password"
+          />
+
+          <button type="submit" value="Log In" data-test="submit">Login
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
