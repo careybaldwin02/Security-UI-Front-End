7b640248ed0dcc91005d8686dab23899fd470140
Carey <careycbaldwin@gmail.com>
2019-02-02 22:11:04 -0500

    added profile of Chuck Santiago

--- security/src/PerpDemo.js
+++ security/src/PerpDemo.js
@@ -3,46 +3,36 @@ import { Link } from 'react-router-dom';
 import SideBar from './SideBar.js';
 import Header from './Header';
 import './App.css';
+import blacklistprofile from './img/blacklistprofile.jpg';
 
 
 class Settings extends Component {
-  render() {
-    return (
-    <div className = "container">
-        <div>
-        <SideBar />
-        <Header />
-        </div>
-        <div className = "page-wrap">
-        <div className="perp-demo">
-
-        <h1>Profile of John Doe</h1>
-            <ul>
-                <li>    
-                    Current Location          
-                </li>
-                <li>    
-                    Share Information            
-                </li>
-                <li>
-                    {/* link to list of names, one name links to black list detail fig 8*/}
-                    Add to Watchlist 
-                </li>
-                    {/* link to list of names */}
-                    Add to Whitelist
-                <li>
-                    {/* link to list control */}
-                    <i class="fas fa-shopping-cart">  </i>
-                     Purchase Information
-                </li>
-            </ul>
+    render() {
+        return (
+            <div className="container">
+                <div>
+                    <SideBar />
+                    <Header />
+                </div>
+                {/* <h1>Profile of Chuck Santiago</h1> */}
+                <div className="perp-demo">
+                    <div className="perp-demo-chart">
+                        <img src={blacklistprofile} />
+                    </div>
+                    <div className="perp-demo-list">
+                        <ul>
+                            <li>Current Location</li>
+                            <li>Share Information</li>
+                            <li>Add to Watchlist</li>
+                            <li>Add to Whitelist</li>
+                            <li><i class="fas fa-shopping-cart"></i>Purchase Information</li>
+                        </ul>
+                    </div>
+                </div>
 
             </div>
-        </div>
-
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default Settings;
--- security/src/SavedSearches.js
+++ security/src/SavedSearches.js
@@ -1,51 +1,40 @@
-import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
-import SideBar from './SideBar';
-import Header from './Header';
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import SideBar from "./SideBar";
+import Header from "./Header";
 
-import './App.css';
-
+import "./App.css";
 
 class SavedSearches extends Component {
   render() {
     return (
-    <div className = "container">
+      <div className="container">
         <div>
-            <SideBar />
-            <Header />
+          <SideBar />
+          <Header />
         </div>
-      <div className="recent">
-        <h1>
-          Saved Searches
-        </h1>
-        <div className = "recent-item">
-        <Link to = {'/perp-demo'}>
-        <button>view</button>
-        </Link>
-        <p>John Doe, 406 E. Jefferson St. Ann Arbor MI 48104            
-        </p>
-        
-        </div>
-        <div className = "recent-item">
-        <button>view</button>
-        <p>
-            06117
-        </p>
-   
-        </div>
-        <div className = "recent-item">
-        <button>view</button>
-        <p>
-            West Hartford, CT
-        </p>
-
-        </div>
-        <div className = "recent-item">
-        <button>view</button>
-        <p>
-            Massachusetts
-        </p>
-        </div>
+        <div className="recent">
+          <h1>Saved Searches</h1>
+          <div className="recent-item">
+            <Link to={"/perp-demo"}>
+              <button>
+                <b>view</b>
+              </button>
+            </Link>
+            <h3>Chuck Santiago, 406 E. Jefferson St. Ann Arbor MI 48104</h3>
+          </div>
+          <div className="recent-item">
+            <button>view</button>
+            <h3>06117</h3>
+          </div>
+          <div className="recent-item">
+            <button>view</button>
+            <h3>West Hartford, CT</h3>
+          </div>
+          <div className="recent-item">
+            <button>view</button>
+            <h3>Massachusetts</h3>
+          </div>
         </div>
       </div>
     );
