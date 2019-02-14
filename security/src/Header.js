2960e746dc3b1b8d8fb72692f6ae1ce117b2084d
Carey <careycbaldwin@gmail.com>
2019-02-14 13:00:15 -0500

    modal expansion for action center

--- security/src/Header.js
+++ security/src/Header.js
@@ -1,60 +1,64 @@
-import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
-import SideBar from './SideBar';
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import SideBar from "./SideBar";
 
-const Header = () => {
-    return (
-        <div className="header">
+class Header extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isHidden: true // for map controls
+        };
+    }
 
-            <Link to={'/favorites'}>
-                <div className="favorites-icon">
-                    <i className="fas fa-star"></i>
-                </div>
-            </Link>
+    toggleHidden() {
+        this.setState({
+            isHidden: !this.state.isHidden
+        });
+    }
 
-            <div className="perptrac">
-                <Link to="/main">
-                    <div className="title-logo">
-                        <h1>TrailTracker</h1>
-                        {/* <i class="fas fa-map-marker-alt faa-pulse animated"></i> */}
-                        <i class="fas fa-map-pin faa-pulse animated"></i>
+    render() {
+        return (
+            <div className="header">
+                <Link to={"/favorites"}>
+                    <div className="favorites-icon">
+                        <i className="fas fa-star" />
                     </div>
-                    <p>
-                        The Next Generation of Physical Surveillance</p>
                 </Link>
-            </div>
 
-            <div className="amazon">
-                <a href={'http://www.amazon.com'} target="_blank">
-                    <i class="fab fa-amazon"></i>
-                </a>
-            </div>
+                <div className="perptrac">
+                    <Link to="/main">
+                        <div className="title-logo">
+                            <h1>TrailTracker</h1>
+                            {/* <i class="fas fa-map-marker-alt faa-pulse animated"></i> */}
+                            <i class="fas fa-map-pin faa-pulse animated" />
+                        </div>
+                        <p>The Next Generation of Physical Surveillance</p>
+                    </Link>
+                </div>
 
-            <div className="video">
-                <i class="fas fa-video"></i>
+                <div className="amazon">
+                    <a href={"http://www.amazon.com"} target="_blank">
+                        <i class="fab fa-amazon" />
+                    </a>
+                </div>
+                <div>
+                    <i
+                        onClick={this.toggleHidden.bind(this)}
+                        className="fas fa-ellipsis-v"
+                    />
+                </div>
+                {!this.state.isHidden && (
+                    <div className="dropdown-modal-container">
+                        <div className = "dropdown-modal">
+                            <i class="fas fa-video" />
+                            <i class="fas fa-bell" />
+                            <i className="fas fa-phone"></i>
+                        </div>
+                    </div>
+                )}
             </div>
-
-            <div className="alarm">
-                {/* <Link to={'/alarm'}> */}
-                    {/* <div className = "alarm">
-                Home Alarm
-            </div> */}
-                    <i class="fas fa-bell"></i>
-                    {/* <i className="fas fa-house-damage"></i> */}
-                {/* </Link> */}
-            </div>
-            <div className="emergency">
-                {/* <Link to={'/emergency'}> */}
-                    <i className="fas fa-phone"> 911</i>
-                {/* </Link> */}
-            </div>
-
-
-            <div>
-            </div>
-        </div>
-    )
-};
-
+        );
+    }
+}
 
 export default Header;
